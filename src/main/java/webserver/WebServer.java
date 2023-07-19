5b102466a5d03388ec20ba2e7b2b0b91df1e27dd
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2023-07-19 17:33:00 +0900

    Revert "refactor: Thread실행을 CompletableFuture로 변경"
    This reverts commit 512b08ee660e0a31f84be341483745d2ea2b10ee.

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,9 +2,6 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,8 +18,6 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-        final ExecutorService executorService = Executors.newFixedThreadPool(100);
-
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -30,8 +25,8 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                final CompletableFuture<Void> future = CompletableFuture.runAsync(new RequestHandler(connection), executorService);
-                future.get();
+                Thread thread = new Thread(new RequestHandler(connection));
+                thread.start();
             }
         }
     }
